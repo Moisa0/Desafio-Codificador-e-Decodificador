01143930c18bf7a644f9b071091d4a9f5818797e
Moisa <telesmoises8@gmail.com>
2024-02-21 01:28:36 -0300

    atualizações do javascript

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-let textoPuro = document.querySelector("")
+let textoBotaoDinamico = document.querySelector(".texto__função")
+
+textoBotaoDinamico.innerHTML= ""
+
+function permanecerPressionado(){
+
+       
+}
+
+function criptografarTexto(texto) {
+    const textoCriptografado = 
+    texto.replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "orber")
+    .replace(/u/g, "ufat")
+ 
+    return textoCriptografado
+ }
+ 
+ function descriptografarTexto(texto) {
+    const textoDescriptografado = 
+    texto.replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/orber/g, "o")
+    .replace(/ufat/g, "u")
+ 
+    return textoDescriptografado
+ }
+ 
